8cd5ee40b116c66f7e0aaf5df6d106eb08ef2548
anil <anilvelayudam@gmail.com>
2025-02-04 12:34:16 +0530

    arra python code

---
+++ arr.py
@@ -0,0 +1,22 @@
+# a=[10,20,30,40]
+# b=[20,30,50,60,70]
+# a1=set(a)
+# b1=set(b)
+# c1=a1.intersection(b1)
+# c=list(c1)
+# print(c)
+
+
+# a = [10, 20, 30, 40]
+# b = [20, 30, 50, 60, 70]
+
+# c = [element for element in a if element in b]
+
+# print("Common elements:", c)
+
+# a = [10, 20, 30, 40]
+# b = [20, 30, 50, 60, 70]
+
+# c = list(set(a) & set(b))
+
+# print("Common elements:", c
