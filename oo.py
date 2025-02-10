71b08627ecd1ce5d96914f7352aa8130e9ae5c55
anil <anilvelayudam@gmail.com>
2025-02-10 15:50:52 +0530

    baen 2

---
+++ oo.py
@@ -0,0 +1,15 @@
+print("Sum= {}".format(float(input("Enter FirstValue:"))+float(input("Enter Second Value:"))))
+
+
+print("Sum= {}".format(float(input("Enter FirstValue:"))+float(input("Enter Second Value:"))))
+
+
+print("Sum= {}".format(float(input("Enter FirstValue:"))+float(input("Enter Second Value:"))))
+
+
+player3=TP.difference(CP)
+print(player3)
+
+#Find all the players who are playing Exclusively Cricket and Tennis----
+player4=TP.symmetric_difference(CP)
+print(player4)
